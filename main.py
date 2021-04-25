cd45ce173fe3208bd155894470c1be4e1b509287
Teapare <teapare@outlook.com>
2021-04-25 14:11:06 +0500

    added elements of restful API and sqlalchemy

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from flask import Flask, render_template
+from flask_restful import reqparse, abort, Api, Resource
+from data import db_session
 
 app = Flask(__name__)
+api = Api(app)
 app.config['SECRET_KEY'] = "Hello"
 
 
@@ -10,6 +13,7 @@ def index():
 
 
 def main():
+    db_session.global_init("db/formulas.db")
     app.run()
 
 
